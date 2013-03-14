4e8561d7cedd6994dd1843dba1f2669b666fb10c
Diallo Abdourahamane <abdd.diallo@gmail.com>
2013-03-14 15:17:52 +0100

    #1 ajout du projet maven

---
+++ ws_covtoirage/src/main/java/iaws/dia/ws_covtoirage/App.java
@@ -0,0 +1,13 @@
+package iaws.dia.ws_covtoirage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
